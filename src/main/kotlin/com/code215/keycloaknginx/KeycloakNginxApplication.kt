717a3ffc5e51fc5cc1e4baf62697208603c437f7
Michael Wangia <michael.w@getslatewell.com>
2023-03-06 00:35:20 -0500

    first commit

---
+++ src/main/kotlin/com/code215/keycloaknginx/KeycloakNginxApplication.kt
@@ -0,0 +1,11 @@
+package com.code215.keycloaknginx
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KeycloakNginxApplication
+
+fun main(args: Array<String>) {
+	runApplication<KeycloakNginxApplication>(*args)
+}
---
+++ src/test/kotlin/com/code215/keycloaknginx/KeycloakNginxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.code215.keycloaknginx
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KeycloakNginxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
